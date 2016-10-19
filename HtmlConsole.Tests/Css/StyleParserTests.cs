3cff52c4c30837c61f6c7031da63d9ca01ee424a
Matěj Zábský <mzabsky@gmail.com>
2016-10-19 23:12:49 +0200

    Style parser now passes the simplest sanity check

---
+++ HtmlConsole.Tests/Css/StyleParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlConsole.Css;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HtmlConsole.Tests.Css
+{
+    [TestClass]
+    public class StyleParserTests
+    {
+        [TestMethod]
+        public void ParseNode_SingleNode_ParsesCorrectly()
+        {
+            var parser = new StyleParser();
+        }
+    }
+}
---
+++ HtmlConsole/Css/StyleParser.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Reflection;
+using Eto.Parse;
+using Eto.Parse.Grammars;
+
+namespace HtmlConsole.Css
+{
+    public class StyleParser
+    {
+        private Grammar _grammar;
+
+        public StyleParser()
+        {
+            var a = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+             var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "HtmlConsole.Css.cssgrammar.txt";
+
+            string grammarText;
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                grammarText = reader.ReadToEnd();
+            }
+
+            _grammar = new EbnfGrammar(EbnfStyle.W3c).Build(grammarText, "stylesheet");
+        }
+
+        public void Parse(string str)
+        {
+            var match = _grammar.Match("");
+        }
+    }
+}
