0546d28a08417bc992a132b539be8a4cf54f4719
Matěj Zábský <mzabsky@gmail.com>
2016-10-29 11:25:39 +0200

    The css parser test now actually checks the produced syntax tree

--- HtmlConsole.Tests/Css/StyleParserTests.cs
+++ HtmlConsole.Tests/Css/StyleParserTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlConsole.Css;
+using HtmlConsole.Extensions;
+using HtmlConsole.Tests.Css.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HtmlConsole.Tests.Css
@@ -11,11 +13,71 @@ namespace HtmlConsole.Tests.Css
     [TestClass]
     public class StyleParserTests
     {
-        [TestMethod]
-        public void ParseNode_SingleNode_ParsesCorrectly()
+        // The parser is immutable
+        private readonly TestingStyleParser _parser = new TestingStyleParser();
+
+        private void TestGetSyntaxTree(string expectedTreeString, string code)
         {
-            var parser = new StyleParser();
-            parser.Parse("div#hash{background:red;}     #someid{padding:1px 1px;}");
+            var syntaxTree = _parser.TestingGetSyntaxTree(code);
+            var actualTreeString = _parser.PrintSyntaxTree(syntaxTree,
+                (match, level) =>
+                    $"{new string('\t', level)}{(match.Matches.Any() ? match.Name : match.Name + "=" + match.Text)}{Environment.NewLine}");
+
+            var expectedTreeLines = expectedTreeString.SplitIntoLines().Where(p => !string.IsNullOrWhiteSpace(p)).ToArray();
+            var actualTreeLines = actualTreeString.SplitIntoLines().Where(p => !string.IsNullOrWhiteSpace(p)).ToArray();
+
+            int lineNumber = 0;
+            foreach (var linesPairs in expectedTreeLines.Zip(actualTreeLines))
+            {
+                if(linesPairs.Item1.Trim() != linesPairs.Item2.Trim())
+                {
+                    Assert.Fail($"Tree line {lineNumber} was expected to be \"{linesPairs.Item1.Trim()}\", was \"{linesPairs.Item2.Trim()}\". Full tree:{Environment.NewLine}{Environment.NewLine}{actualTreeString}");
+                }
+                lineNumber++;
+            }
+
+            if (expectedTreeLines.Length != actualTreeLines.Length)
+            {
+                Assert.Fail($"Tree line number was expected to be \"{expectedTreeLines.Length}\", was \"{actualTreeLines.Length}\". Full tree:{Environment.NewLine}{Environment.NewLine}{actualTreeString}");
+            }
+        }
+
+        [TestMethod]
+        public void GetSyntaxTree_SimpleCss_ParsesCorrectly()
+        {
+            TestGetSyntaxTree(
+                @"
+                stylesheet
+                    ruleset
+                        selectors
+                            selector
+                                element_name
+                                    ident=div
+                                hash
+                                    ident=hash
+                        declarations
+                            declaration
+                                ident=background
+                                expression
+                                    term
+                                        ident=red
+                        S=     
+                    ruleset
+                        selectors
+                            selector
+                                hash
+                                    ident=someid
+                        declarations
+                            declaration
+                                ident=padding
+                                expression
+                                    term
+                                        number=1px
+                                    operator
+                                        S= 
+                                    term
+                                        number=1px",
+                "div#hash{background:red;}     #someid{padding:1px 1px;}");
         }
     }
 }
