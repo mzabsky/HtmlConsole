ae77c84357bb194f96d5f4bfb44d0b6cc918eeef
Matěj Zábský <mzabsky@gmail.com>
2016-10-30 16:05:26 +0100

    Added missing testing class.

---
+++ HtmlConsole.Tests/Css/Testing/TestingStyleParser.cs
@@ -0,0 +1,13 @@
+﻿using Eto.Parse;
+using HtmlConsole.Css;
+
+namespace HtmlConsole.Tests.Css.Testing
+{
+    public class TestingStyleParser : StyleParser
+    {
+        public Match TestingGetSyntaxTree(string str)
+        {
+            return GetSyntaxTree(str);
+        }
+    }
+}
