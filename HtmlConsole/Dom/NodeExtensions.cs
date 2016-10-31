7572576c05b95ace7c8f8d3633ab48a4a6368bb7
Matěj Zábský <mzabsky@gmail.com>
2016-10-31 18:59:47 +0100

    Added GetSiblings() method to Node.

---
+++ HtmlConsole/Dom/NodeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HtmlConsole.Dom
+{
+    public static class NodeExtensions
+    {
+        public static IEnumerable<INode> GetSiblings(this INode node)
+        {
+            if (node.Parent == null)
+            {
+                return new INode[0];
+            }
+
+            return node.Parent.Children.Where(p => p != node);
+        }
+    }
+}
