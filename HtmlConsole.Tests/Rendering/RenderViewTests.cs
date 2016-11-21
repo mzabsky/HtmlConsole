00037695328e2af6e67c8b475349e9ee0a088b72
Matěj Zábský <mzabsky@gmail.com>
2016-11-21 23:09:02 +0100

    Implemented basic rendering tree construction.

---
+++ HtmlConsole.Tests/Rendering/RenderViewTests.cs
@@ -0,0 +1,21 @@
+﻿using HtmlConsole.Dom;
+using HtmlConsole.Rendering;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HtmlConsole.Tests.Rendering
+{
+    [TestClass]
+    public class RenderViewTests
+    {
+        [TestMethod]
+        public void Construct_SimpleDocument_CreatesAppropriateView()
+        {
+            var document = Document.ParseHtml("<xyz/>");
+            document.ComputeStyles();
+            var view = new RenderView(document);
+
+            Assert.AreEqual(document, view.Document);
+            Assert.AreEqual(typeof(InlineRenderer), view.RootRenderer.GetType());
+        }
+    }
+}
---
+++ HtmlConsole/Rendering/Renderer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using HtmlConsole.Dom;
+
+namespace HtmlConsole.Rendering
+{
+    /// <summary>
+    /// A node in the rendering tree, based in the <see cref="RenderView"/>.
+    /// </summary>
+    public abstract class Renderer
+    {
+        public INode DomNode { get; }
+        public Renderer Parent { get; set; }
+        public List<Renderer> Children { get; set; } = new List<Renderer>();
+
+        // reference na renderview?
+        // aspect ratio elements?
+        // Continuations?
+        // Anonymous blocks
+        // enclosing box (utility only )
+
+        protected Renderer(INode domNode, Renderer parent = null)
+        {
+            Parent = parent;
+            DomNode = domNode;
+        }
+    }
+
+    public class ElementRenderer : Renderer
+    {
+        public ElementRenderer(ElementNode domNode, Renderer parent = null) : base(domNode, parent)
+        {
+        }
+    }
+
+    public class InlineRenderer : ElementRenderer
+    {
+        public InlineRenderer(ElementNode domNode, Renderer parent = null) : base(domNode, parent)
+        {
+        }
+    }
+
+    public class BlockRenderer : ElementRenderer
+    {
+        public BlockRenderer(ElementNode domNode, Renderer parent = null) : base(domNode, parent)
+        {
+        }
+    }
+
+    public class TextRenderer : Renderer
+    {
+        public TextRenderer(TextNode domNode, Renderer parent = null) : base(domNode, parent)
+        {
+        }
+    }
+
+    public class ReplacedContentRenderer : Renderer
+    {
+        public ReplacedContentRenderer(ElementNode domNode, Renderer parent = null) : base(domNode, parent)
+        {
+        }
+    }
+
+    public class VoidRenderer : Renderer
+    {
+        public VoidRenderer(ElementNode domNode, Renderer parent = null) : base(domNode, parent)
+        {
+        }
+    }
+
+    public class RenderView
+    {
+        public Document Document { get; }
+        public Renderer RootRenderer { get; }
+
+        public RenderView(Document document)
+        {
+            Document = document;
+            RootRenderer = document.RootNode.CreateRenderer();
+        }
+    }
+}
